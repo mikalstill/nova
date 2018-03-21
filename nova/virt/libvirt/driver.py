0472bb8b4caa88a8237aaf42ed112e208c70b663
Michael Still <mikal@stillhq.com>
2018-03-21 13:21:22 +1100

    Use a pythonic delete, with a retry.
    Replace an exec() of rm with a more pythonic thing, with a simple
    retry loop now that we can't just use the one oslo gives us for
    free. The retry delay is the same as that currently used by
    oslo.
    
    Change-Id: Icdd039bb4374269d9da38e7f8d2e15e05ca8aadb
    blueprint: execs-ive-had-a-few

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -36,6 +36,7 @@ import itertools
 import operator
 import os
 import pwd
+import random
 import shutil
 import tempfile
 import time
@@ -1215,8 +1216,18 @@ class LibvirtDriver(driver.ComputeDriver):
             # Deletion can fail over NFS, so retry the deletion as required.
             # Set maximum attempt as 5, most test can remove the directory
             # for the second time.
-            utils.execute('rm', '-rf', target, delay_on_retry=True,
-                          attempts=5)
+            attempts = 0
+            while(os.path.exists(target) and attempts < 5):
+                try:
+                    shutil.rmtree(target)
+                except OSError as e:
+                    if e.errno == 2:
+                        # Directory went away in a race
+                        break
+
+                if os.path.exists(target):
+                    time.sleep(random.randint(20, 200) / 100.0)
+                attempts += 1
 
         root_disk = self.image_backend.by_name(instance, 'disk')
         # TODO(nic): Set ignore_errors=False in a future release.
